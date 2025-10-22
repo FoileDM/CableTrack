d4cb7579ad78495a0befe970fccbac56239dc032
Vlad <foile.dm@gmail.com>
2025-10-22 15:27:57 +0300

    style(catalog): update style

--- src/apps/catalog/admin.py
+++ src/apps/catalog/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+
 from apps.catalog.models import CableModel, Drum
+
 
 @admin.register(CableModel)
 class CableModelAdmin(admin.ModelAdmin):
     list_display = ("code", "name", "min_length_m", "max_length_m", "created_at")
     search_fields = ("code", "name")
+
 
 @admin.register(Drum)
 class DrumAdmin(admin.ModelAdmin):
