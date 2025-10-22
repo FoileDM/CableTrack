02099fda8b39aaa6ca0952ad1595a7a876d59820
Vlad <foile.dm@gmail.com>
2025-10-22 15:31:51 +0300

    feat(storage): add models, migrations, admin for storage

--- src/apps/storage/admin.py
+++ src/apps/storage/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.storage.models import Storage
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "created_at")
+    search_fields = ("code",)
--- src/apps/storage/models.py
+++ src/apps/storage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from apps.core.models import TimeStampedModel
+
+
+class Storage(TimeStampedModel):
+    code = models.CharField("Код места хранения", max_length=64, unique=True)
+    name = models.CharField("Название", max_length=128, blank=True, default="")
+
+    class Meta:
+        verbose_name = "Склад"
+        verbose_name_plural = "Склады"
+
+    def save(self, *args, **kwargs):
+        if self.code: self.code = self.code.strip().upper()
+        super().save(*args, **kwargs)
+
+    def __str__(self): return self.code
