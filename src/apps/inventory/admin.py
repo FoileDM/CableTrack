0d45914da9e556bf55de2ff05c436248e9b9f7b4
Vlad <foile.dm@gmail.com>
2025-10-23 09:31:35 +0300

    feat(inventory): change admin form for upload view on add Batch

--- src/apps/inventory/admin.py
+++ src/apps/inventory/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import path, reverse
 
 from apps.inventory.models import Batch, BatchItem
+from apps.inventory.views import BatchImportAdminView
 
 
 @admin.register(Batch)
 class BatchAdmin(admin.ModelAdmin):
     list_display = ("number", "created_at")
     search_fields = ("number",)
+    ordering = ("-created_at",)
+
+    def add_view(self, request, form_url="", extra_context=None):
+        return redirect(reverse("admin:inventory_batch_import"))
+
+    def get_urls(self):
+        urls = super().get_urls()
+        view = BatchImportAdminView.as_view(admin_site=self.admin_site)
+        my_urls = [
+            path(
+                "import/",
+                self.admin_site.admin_view(view),
+                name="inventory_batch_import",
+            ),
+        ]
+        return my_urls + urls
 
 
 @admin.register(BatchItem)
